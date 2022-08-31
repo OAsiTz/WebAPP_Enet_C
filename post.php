a15e1a2effa613169efc738513407c48b930d5b4
OAsiTz <teven147q@gmail.com>
2022-08-31 11:06:13 +0700

    work3

---
+++ post.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1><center>Webboard OAsiTz</center></h1>
+<hr>
+<center> ต้องการดูกระทู้หมายเลข 3 <br><br>
+<table style="border:2px solid black;width:20%">
+        <tr><td style="background-color: #6cd2fe;"colspan="2">แสดงความคิดเห็น</td></tr>
+        <tr><td><input type="user"name="user" size ="100"></td></tr>
+        <tr><td colspan ="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+</table>
+</center>
+<br>
+<center><a href="index.html">กลับไปหน้าหลัก</a></center>
+</body>
+</html>
